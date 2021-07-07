4c2325e42fbaa1950ba218261fa53d46e11f7ee2
山室 友樹 <yuki.sumaho@gmail.com>
2021-07-08 08:24:39 +0900

    create form

--- login.gtpl
+++ login.gtpl
@@ -1,12 +1,16 @@
 <html>
-<head>
-<title></title>
-</head>
-<body>
-<form action="/login" method="post">
-    ユーザ名:<input type="text" name="username">
-    パスワード:<input type="password" name="password">
-    <input type="submit" value="ログイン">
-</form>
-</body>
+  <head>
+    <title>goでWebサイト作成</title>
+  </head>
+  <body>
+    <form action="/login" method="post">
+      <input type="checkbox" name="interest" value="football">サッカー
+      <input type="checkbox" name="interest" value="basketball">バスケットボール
+      <input type="checkbox" name="interest" value="tennis">テニス
+      ユーザ名:<input type="text" name="username">
+      パスワード:<input type="password" name="password">
+      <input type="hidden" name="token" value="{{.}}">
+      <input type="submit" value="ログイン">
+    </form>
+  </body>
 </html>
--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/md5"
 	"fmt"
+	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
@@ -20,8 +25,41 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World!")
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method)
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			if len(r.Form["username"][0]) < 5 {
+				fmt.Println("len:", len(r.Form["username"]))
+				t, _ := template.ParseFiles("login.gtpl")
+				t.Execute(w, nil)
+			} else {
+				fmt.Println("username:", r.Form["username"])
+				fmt.Println("password:", r.Form["password"])
+			}
+		} else {
+			//エラー処理
+		}
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username")))
+	}
+}
+
 func main() {
 	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
 	err := http.ListenAndServe(":9090", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
