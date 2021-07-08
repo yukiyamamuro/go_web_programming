360ccd287cb2ca05e122ae8e4fc497dbadbb7600
山室 友樹 <yuki.sumaho@gmail.com>
2021-07-09 08:22:34 +0900

    写経

--- main.go
+++ main.go
@@ -1,102 +1,48 @@
 package main
 
 import (
-	"crypto/md5"
-	"fmt"
-	"html/template"
-	"io"
-	"log"
 	"net/http"
-	"os"
-	"strconv"
-	"strings"
 	"time"
 )
 
-func sayhelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	expiration := time.Now()
-	expiration = expiration.AddDate(1, 0, 0)
-	cookie := http.Cookie{Name: "name", Value: "nanasi", Expires: expiration}
-	http.SetCookie(w, &cookie)
-	fmt.Fprintf(w, "Hello World!")
-}
-
-func login(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method)
-	if r.Method == "GET" {
-		crutime := time.Now().Unix()
-		h := md5.New()
-		io.WriteString(h, strconv.FormatInt(crutime, 10))
-		token := fmt.Sprintf("%x", h.Sum(nil))
-
-		t, _ := template.ParseFiles("login.gtpl")
-		t.Execute(w, token)
-	} else {
-		r.ParseForm()
-		token := r.Form.Get("token")
-		if token != "" {
-			if len(r.Form["username"][0]) < 5 {
-				fmt.Println("len:", len(r.Form["username"]))
-				t, _ := template.ParseFiles("login.gtpl")
-				t.Execute(w, nil)
-			} else {
-				fmt.Println("username:", r.Form["username"])
-				fmt.Println("password:", r.Form["password"])
-			}
-		} else {
-			//エラー処理
-		}
-		fmt.Println("username length:", len(r.Form["username"][0]))
-		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username")))
-		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
-		template.HTMLEscape(w, []byte(r.Form.Get("username")))
-	}
-}
-
-func upload(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("method:", r.Method)
-	if r.Method == "GET" {
-		crutime := time.Now().Unix()
-		h := md5.New()
-		io.WriteString(h, strconv.FormatInt(crutime, 10))
-		token := fmt.Sprintf("%x", h.Sum(nil))
-
-		t, _ := template.ParseFiles("upload.gtpl")
-		t.Execute(w, token)
-	} else {
-		r.ParseMultipartForm(32 << 20)
-		file, handler, err := r.FormFile("uploadfile")
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer file.Close()
-		fmt.Fprintf(w, "%v", handler.Header)
-		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer f.Close()
-		io.Copy(f, file)
-	}
-}
-
 func main() {
-	http.HandleFunc("/", sayhelloName)
-	http.HandleFunc("/login", login)
-	http.HandleFunc("/upload", upload)
-	err := http.ListenAndServe(":9090", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	p("ChitChat", version(), "started at", config.Address)
+
+	// handle static assets
+	mux := http.NewServeMux()
+	files := http.FileServer(http.Dir(config.Static))
+	mux.Handle("/static/", http.StripPrefix("/static/", files))
+
+	//
+	// all route patterns matched here
+	// route handler functions defined in other files
+	//
+
+	// index
+	mux.HandleFunc("/", index)
+	// error
+	mux.HandleFunc("/err", err)
+
+	// defined in route_auth.go
+	mux.HandleFunc("/login", login)
+	mux.HandleFunc("/logout", logout)
+	mux.HandleFunc("/signup", signup)
+	mux.HandleFunc("/signup_account", signupAccount)
+	mux.HandleFunc("/authenticate", authenticate)
+
+	// defined in route_thread.go
+	mux.HandleFunc("/thread/new", newThread)
+	mux.HandleFunc("/thread/create", createThread)
+	mux.HandleFunc("/thread/post", postThread)
+	mux.HandleFunc("/thread/read", readThread)
+
+	// starting up the server
+	server := &http.Server{
+		Addr:           config.Address,
+		Handler:        mux,
+		ReadTimeout:    time.Duration(config.ReadTimeout * int64(time.Second)),
+		WriteTimeout:   time.Duration(config.WriteTimeout * int64(time.Second)),
+		MaxHeaderBytes: 1 << 20,
 	}
+	server.ListenAndServe()
 }
